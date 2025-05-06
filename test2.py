8af093b9e7fd29bdd4cc0b92011279fd5a078874
MrE8065 <102242666+MrE8065@users.noreply.github.com>
2025-05-06 17:55:54 +0200

    Created the inside-anime view

--- test2.py
+++ test2.py
@@ -1,38 +1,32 @@
 import flet as ft
+from search import get_anime
 
-def main(page: ft.Page):
-    page.title="AniPy"
+def detalle_view(page: ft.Page):
+    anime_link = page.client_storage.get("anime_link")
+    titulo, sinopsis, valoracion, imagen = get_anime(anime_link)
 
-    imagen=ft.Image(src="https://www3.animeflv.net/uploads/animes/covers/4173.jpg")
-    desc=ft.Text("Lorem ipsum dolor sit amet consectetur adipiscing elit suspendisse fermentum, pellentesque parturient sociosqu litora class lobortis facilisi urna ullamcorper morbi, gravida odio congue enim ut nulla cum eu.\n Curabitur consequat netus lobortis felis himenaeos cubilia semper nisi at tortor, porta dictum donec sagittis nostra parturient risus dapibus odio per, a elementum integer arcu blandit sodales nibh fringilla mus.\n Condimentum suscipit viverra fermentum parturient libero eros nec et, eget magnis class dis dignissim quis maecenas quam, rhoncus arcu velit fusce donec in ante. Turpis litora ridiculus curabitur ad nibh lacinia proin pellentesque rutrum conubia, nisl praesent accumsan ligula phasellus convallis cubilia cum montes, dictum a scelerisque dictumst vivamus arcu eget at aptent.\n Fusce interdum aliquet curae ligula iaculis nec neque ultrices vel, scelerisque fames semper sed mi commodo natoque est netus, euismod id pharetra congue fermentum pellentesque rhoncus maecenas.\n Curabitur dictumst vel dui in ante mi himenaeos, eu malesuada potenti ligula sem iaculis faucibus semper, sagittis quisque morbi ornare ac mattis.", expand=True)
+    titulo_txt = ft.Text(titulo, weight=ft.FontWeight.BOLD)
+    imagen = ft.Image(src=imagen)
+    desc = ft.Text(sinopsis)
+    val=ft.Text(valoracion+"⭐")
+    eps=ft.ListView()
 
-    page.add(
-        ft.Container(
-            content=(
-                ft.Row(
-                    controls=[
-                            imagen,
-                            ft.Container(
-                                content=ft.Column(
-                                    controls=[
-                                        ft.Container(desc, border=ft.border.all(2, ft.Colors.RED)),
-                                        ft.Container(
-                                            ft.Column(),
-                                            border=ft.border.all(2, ft.Colors.BLUE), expand=True
-                                        )
-                                    ],
-                                    alignment=ft.MainAxisAlignment.CENTER
-                                ),
-                                border=ft.border.all(2,ft.Colors.GREEN),
-                                expand=True,
-                                alignment=ft.alignment.center
-                            )
-                        ]
-                )
-            ),
-            border=ft.border.all(2, ft.Colors.AMBER),
-            expand=True
-        )
+    return ft.View(
+        "/detalle",
+        controls=[
+            ft.Row(
+                controls=[
+                    imagen,
+                    ft.Column(
+                        controls=[titulo_txt, val, desc, eps],
+                        expand=True,
+                        alignment=ft.MainAxisAlignment.CENTER,
+                        spacing=20
+                    )
+                ],
+                expand=True,
+                vertical_alignment=ft.CrossAxisAlignment.CENTER,
+                spacing=20
+            )
+        ]
     )
-
-ft.app(main)
